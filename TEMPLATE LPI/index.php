e1e74d0fc040b6d5ef76e4e77cabc9b8bb757f28
Tomas <tomas.santos@ipcbcampus.pt>
2025-05-17 15:34:54 +0100

    Update index.php

--- TEMPLATE LPI/index.php
+++ TEMPLATE LPI/index.php
@@ -114,17 +114,6 @@
                                  <label>Data</label>
                                  <input class="form-control" placeholder="Any" type="date" name="Any">
                               </div>
-
-                              <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
-                                 <label>Pessoas</label>
-                                 <select class="form-control" name="">
-                                    <option>Any</option>
-                                    <option>Option 1</option>
-                                    <option>Option 2</option>
-                                    <option>Option 3</option>
-                                 </select>
-                              </div>
-
                            </div>
                         </div>
                         <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
