a44da67f07cf00dc5fbd5db514def7d5f53412b3
Tomas <tomas.santos@ipcbcampus.pt>
2025-05-18 17:50:49 +0100

    Alas

--- paginas/registo.php
+++ paginas/registo.php
@@ -41,50 +41,66 @@
                         <div class="row g-0">
                             <div class="col-lg-6">
                                 <div class="card-body p-md-5 mx-md-4">
-                                    
                                     <div class="text-center">
                                         <img src="logo.png" class="img-fluid" style="width: 185px;" alt="logo">
                                     </div>
-                                    <form action="index.php" method="POST">
+                                    <form action="index.php" method = "post">
                                         <label class="form-label" for="form2Example11">Utilizador</label>
                                         <div data-mdb-input-init class="form-outline mb-4">
                                             <input type="text" id="form2Example11" class="form-control"
-                                                placeholder="O seu nome de utilizador" />
+                                                placeholder="O seu nome de utilizador" name = "utilizador"/>
                                         </div>
+
+                                        <label class="form-label" for="form2Example11">Email</label>
+                                        <div data-mdb-input-init class="form-outline mb-4">
+                                            <input type="email" id="form2Example11" class="form-control"
+                                                placeholder="O seu email" name = "email"/>
+                                        </div>
+
                                         <label class="form-label" for="form2Example22">Password</label>
                                         <div data-mdb-input-init class="form-outline mb-4">
                                             <input type="password" id="form2Example22" class="form-control"
-                                                placeholder="A sua password" />
+                                                placeholder="A sua password" name = "password"/>
+                                        </div>
+
+                                        <label class="form-label" for="form2Example22">Repita a Password</label>
+                                        <div data-mdb-input-init class="form-outline mb-4">
+                                            <input type="password" id="form2Example22" class="form-control"
+                                                placeholder="A sua password" name = "confirmar_password" />
                                         </div>
                                         <div class="text-center pt-1 mb-5 pb-1">
                                             <button data-mdb-button-init data-mdb-ripple-init
                                                 class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
-                                                type="button" style="height: 40px;">
-                                                Login
+                                                type="submit" style="height: 40px;">
+                                                Registar
                                             </button>
 
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-between pb-4">
-                                                <p class="mb-0 me-2">Não tens conta?</p>
-                                                <a href="registo.html" type="button"  data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-primary">Regista-te!</a>
-                                            </div>
-                                        </form>
-                                    </div>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-between pb-4">
+                                            <p class="mb-0 me-2">Já tens conta?</p>
+                                            <a href="login.php" type="button" data-mdb-button-init data-mdb-ripple-init
+                                                class="btn btn-outline-primary">Faz Login!</a>
+                                        </div>
+                                    </form>
                                 </div>
-                                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                        <h1 class="mb-4">Queres viajar à <b>baizis</b>?</h1>
-                                        <p class="medium mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        </p>
-                                    </div>
+                            </div>
+                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                    <h1 class="mb-4">Queres viajar à <b>baizis</b>?</h1>
+                                    <p class="medium mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                                        do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud
+                                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
-    </body>
+        </div>
+    </section>
+</body>
+
 </html>
